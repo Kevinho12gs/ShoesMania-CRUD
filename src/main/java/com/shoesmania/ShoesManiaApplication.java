aaa4092d4415ec13f698945f5d120bc4b2eafbf0
Kevinho12gs <129360050+Kevinho12gs@users.noreply.github.com>
2025-01-15 19:16:33 -0500

    Agregando ShoesManiaApplication.java

---
+++ src/main/java/com/shoesmania/ShoesManiaApplication.java
@@ -0,0 +1,11 @@
+package com.shoesmania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesManiaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShoesManiaApplication.class, args);
+    }
+}
